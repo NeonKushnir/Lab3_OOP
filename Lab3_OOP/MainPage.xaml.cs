e076638f60a2a36d4a45f1a014f9e032e1ed5bfc
User <User@DESKTOP-43OCBND>
2023-12-07 20:38:43 +0200

    Добавьте файлы проекта.

---
+++ Lab3_OOP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Collections.ObjectModel;
+
+public class MyDataItem
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class MainPageViewModel
+{
+    public ObservableCollection<MyDataItem> Items { get; set; }
+
+    public MainPageViewModel()
+    {
+        Items = new ObservableCollection<MyDataItem>();
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        string jsonFilePath = @"C:\Users\User\Desktop\courses.json";
+
+        if (File.Exists(jsonFilePath))
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            var items = JsonSerializer.Deserialize<List<MyDataItem>>(json);
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+        }
+    }
+}
---
+++ Lab3_OOP/models.cs
@@ -0,0 +1,37 @@
+﻿public class University
+{
+    public List<Department> Department { get; set; }
+}
+
+public class Department
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Id { get; set; }
+    public List<Course> Course { get; set; }
+}
+
+public class Course
+{
+    public string Name { get; set; }
+    public List<Class> Class { get; set; }
+}
+
+public class Class
+{
+    public string Type { get; set; }
+    public string Time { get; set; }
+    public Leader Leader { get; set; }
+    public Instructor Instructor { get; set; }
+}
+
+public class Leader
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+}
+
+public class Instructor
+{
+    public string Name { get; set; }
+}
